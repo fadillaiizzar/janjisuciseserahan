674187a814787b2b238f3d80886e64c72ca9d7ce
fadillaiizzar <fadillaiizzarm@gmail.com>
2025-01-13 22:43:48 +0700

    shadow box project

--- src/Layouts/Project/TotalProject/TotalProject.jsx
+++ src/Layouts/Project/TotalProject/TotalProject.jsx
@@ -2,7 +2,7 @@ export default function TotalProject(props) {
     const { bg="bg-cmaroon" , border="border-r-8" , roundedtr , roundedbr , roundedtl , roundedbl , children , text } = props;
 
     return(
-        <div className={`${bg} ${border} ${roundedtr} ${roundedbr} ${roundedtl} ${roundedbl} text-ccream rounded-xl p-2 sm:p-5 flex flex-col justify-center items-center`}>
+        <div className={`${bg} ${border} ${roundedtr} ${roundedbr} ${roundedtl} ${roundedbl} text-ccream rounded-xl shadow-2xl p-2 sm:p-5 flex flex-col justify-center items-center`}>
             <p className="text-xl sm:text-4xl md:text-5xl mb-2 font-bold">{children}</p>
             <p className="text-sm sm:text-lg">{text}</p>
         </div>
